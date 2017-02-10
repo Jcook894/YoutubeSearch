da47057faa0dd7c7aa25202bca62c83efa6d7642
Jcook894 <jcook894@icloud.com>
2017-02-10 18:16:53 -0500

    Feat: Added a constructor method for the state.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+  
   render () {
-    return <input />;
+
+    return <input onChange={ event => console.log(event.target.value)} />;
   }
 };
 
