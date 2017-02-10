f8116764d1adaaa29e793f728b98c597c4ad80ac
Jcook894 <jcook894@icloud.com>
2017-02-10 01:10:06 -0500

    Feat: Received the Youtube API key.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyC6b4u_uR4Ctvb6Yi9X4JSu_d6ArTsACt4';
+
 const App = function(){
   return <div> Hey, my name is Julian! </div>;
 };
