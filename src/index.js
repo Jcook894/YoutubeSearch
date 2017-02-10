4222aa24549bfca6f97f0bae90a9b26d9b32a967
Jcook894 <jcook894@icloud.com>
2017-02-10 03:13:35 -0500

    Feat: Added input in the searchBar component.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyC6b4u_uR4Ctvb6Yi9X4JSu_d6ArTsACt4';
 
 const App = function(){
-  return <div> Hey, my name is Julian! </div>;
-};
+  return(
+    <div>
+    <SearchBar />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
