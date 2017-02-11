e20692590e7eb999081b9e6a44f64648717e1d3c
Jcook894 <jcook894@icloud.com>
2017-02-10 19:47:34 -0500

    Feat: added state to the event handler, connected state to get the value of the input.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,10 +6,17 @@ class SearchBar extends Component {
 
     this.state = { term: '' };
   }
-  
+
   render () {
 
-    return <input onChange={ event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={ event => this.setState({
+          term: event.target.value
+          })} />
+          Value of the input : {this.state.term}
+      </div>
+  );
   }
 };
 
