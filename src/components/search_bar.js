c54f5e13a2ad66176cce2e8e5c1312777518e27a
Jcook894 <jcook894@icloud.com>
2017-02-10 22:02:39 -0500

    Feat: Added youtube api and a test search to see if working.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,17 +4,19 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
+    this.state = { term: 'Starting Value' };
   }
 
   render () {
 
     return (
       <div>
-        <input onChange={ event => this.setState({
+        <input
+        value={this.state.term}
+        onChange={ event => this.setState({
           term: event.target.value
           })} />
-          Value of the input : {this.state.term}
+
       </div>
   );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+//importing youtube search package.
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyC6b4u_uR4Ctvb6Yi9X4JSu_d6ArTsACt4';
+
+//Youtube api search test.
+YTSearch({key: API_KEY, term:"skate"}, function(data){
+  console.log(data);
+});
 
 const App = function(){
   return(
