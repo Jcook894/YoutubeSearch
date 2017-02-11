ba8149f08c3e02a5c460d5af0ff5cffdefebdac3
Jcook894 <jcook894@icloud.com>
2017-02-11 18:28:30 -0500

    Feat: Added a video list item comp and made a list of the videos.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video)=>{
+    return <VideoListItem videos={video} />
+  });
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+    {videoItems}
     </ul>
 
   );
