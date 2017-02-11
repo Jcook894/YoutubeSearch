3b8293f45fc2e2cc87f115923f6a6b8e4b591f9e
Jcook894 <jcook894@icloud.com>
2017-02-11 00:19:31 -0500

    Feat: Working on video list component.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 //importing youtube search package.
 import YTSearch from 'youtube-api-search';
-
 const API_KEY = 'AIzaSyC6b4u_uR4Ctvb6Yi9X4JSu_d6ArTsACt4';
 
-//Youtube api search test.
-YTSearch({key: API_KEY, term:"skate"}, function(data){
-  console.log(data);
-});
 
-const App = function(){
-  return(
-    <div>
-    <SearchBar />
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [ ] };
+
+    //Youtube api search test.
+    YTSearch({key: API_KEY, term:"skate"},(videos) => {
+      this.setState({videos});
+    });
+
+  }
+  render (){
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
