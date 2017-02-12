03256b2473c25da5c52aef5a25a5f2b603907488
Jcook894 <jcook894@icloud.com>
2017-02-11 22:22:14 -0500

    Feat: Added a key to the VideoListItem component by adding the youtube etag for each video.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,7 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video)=>{
-    return <VideoListItem videos={video} />
+    return <VideoListItem key={video.etag} videos={video} />
   });
   return (
     <ul className="col-md-4 list-group">
